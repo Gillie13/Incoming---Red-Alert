08a2d083670d2dedacffd0d12b8a5958d4f6f761
Jose Gil <josegil@yoursuccess.com.au>
2021-04-27 15:36:43 +0930

    Added Aggresive Enemy Type

---
+++ Assets/Scripts/EnemyFast.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFast : MonoBehaviour
+{
+    private Transform _target;
+    private Rigidbody2D _rigidbody;
+    private float _angleChangingSpeed = 200f;
+    private float _movementSpeed = 5.0f;
+    private float _downSpeed = 4.0f;
+
+    private Player _player;
+    private Animator _anim;
+    private AudioSource _audioSource;
+
+    private SpawnManager _spawnManager;
+
+    public bool _enemyIsDestroyed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _target = GameObject.FindGameObjectWithTag("Player").transform;
+        _rigidbody = GetComponent<Rigidbody2D>();
+
+        _player = GameObject.Find("Player").GetComponent<Player>();
+        if (_player == null)
+        {
+            Debug.LogError("Player is Null");
+        }
+
+        _spawnManager = GameObject.Find("SpawnManager").GetComponent<SpawnManager>();
+        if (_spawnManager == null)
+        {
+            Debug.LogError("Spawn Manger is Null");
+        }
+
+        _audioSource = GetComponent<AudioSource>();
+        if (_audioSource == null)
+        {
+            Debug.LogError("AudioSource is Null");
+        }
+
+        _anim = GetComponent<Animator>();
+        if (_anim == null)
+        {
+            Debug.LogError("Anim is null");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_enemyIsDestroyed == false && _player != null)
+        {
+            if(Vector2.Distance(transform.position, _target.position) > 5f)
+            {
+                Vector2 direction = (Vector2.down) - _rigidbody.position;
+                direction.Normalize();
+                float rotateAmount = Vector3.Cross(direction, -transform.up).z;
+                _rigidbody.angularVelocity = -_angleChangingSpeed * rotateAmount;
+                _rigidbody.velocity = -transform.up * _downSpeed;
+            }
+            else if (Vector2.Distance(transform.position, _target.position) < 5f)
+            {
+                Vector2 direction = (Vector2)_target.position - _rigidbody.position;
+                direction.Normalize();
+                float rotateAmount = Vector3.Cross(direction, -transform.up).z;
+                _rigidbody.angularVelocity = -_angleChangingSpeed * rotateAmount;
+                _rigidbody.velocity = -transform.up * _movementSpeed;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            Player player = other.transform.GetComponent<Player>();
+            if (player != null)
+            {
+                player.Damage();
+            }
+            EnemyDestroyed();
+        }
+
+        if (other.tag == "Laser")
+        {
+            Destroy(other.gameObject);
+            if (_player != null)
+            {
+                _player.AddScore(10);
+            }
+            EnemyDestroyed();
+        }
+
+        if (other.tag == "Missile")
+        {
+            Destroy(other.gameObject);
+            if (_player != null)
+            {
+                _player.AddScore(10);
+            }
+            EnemyDestroyed();
+        }
+    }
+
+    private void EnemyDestroyed()
+    {
+        _enemyIsDestroyed = true;
+        _spawnManager.OnEnemyDeath();
+        _anim.SetTrigger("OnEnemyDeath");
+        _audioSource.Play();
+        Destroy(GetComponent<Collider2D>());
+        Destroy(this.gameObject, 2.8f);
+    }
+
+}
+
